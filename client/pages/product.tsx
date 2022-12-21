b3dd82dbb1834decdba1222c26ae9dfa1674bc5d
Rob Hannan <rob.hannan@red-badger.com>
2022-12-21 15:46:07 +0000

    Add initial top navbar ui

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import Button from '@mui/material/Button';
+import IconButton from "@mui/material/IconButton";
+
+const Navbar = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+`;
 export default function Product() {
-  return <div>Product page</div>;
+  return <div>
+    <Navbar>
+      <Button size="small">
+        <img src='/octopus-logo.svg' alt='octopus logo' style={{ width: 180 }}/>
+      </Button>
+      <IconButton size="small">
+        <img src='/basket.svg' alt='basket' style={{ width: 25 }}/>
+      </IconButton>
+    </Navbar>
+  </div>;
 }
