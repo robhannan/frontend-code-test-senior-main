977954d6fc37447633ea147514bea18f9b48e135
Rob Hannan <rob.hannan@red-badger.com>
2022-12-30 17:15:41 +0000

    Add test for disabled - button Add test for adding multiple batches to basket

--- client/test/product.test.tsx
+++ client/test/product.test.tsx
@@ -101,3 +101,49 @@ test("should show full page content instead of loading screen if product info is
   expect(queryByText('Loading...')).toBeNull();
   expect(queryByTitle('Product')).toBeTruthy();
 });
+
+test("should not be able to decrease product quantity below 1", async() => {
+  const { findByText, getByText, getByTitle } = render(
+    <MockedProvider mocks={mocks}>
+      <Product />
+    </MockedProvider>
+  );
+
+  await findByText("Product Name");
+
+  const decreaseQuantity = getByText("-");
+
+  const currentQuantity = getByTitle("Current quantity");
+  expect(currentQuantity).toHaveTextContent("1");
+  expect(decreaseQuantity).toBeDisabled();
+
+  fireEvent.click(decreaseQuantity);
+  expect(currentQuantity).toHaveTextContent("1");
+});
+
+test("should be able to add multiple batches of items to the basket and combine the quantites", async() => {
+  const { findByText, getByText, getByTitle } = render(
+    <MockedProvider mocks={mocks}>
+      <Product />
+    </MockedProvider>
+  );
+
+  await findByText("Product Name");
+  const increaseQuantity = getByText("+");
+
+  fireEvent.click(increaseQuantity);
+  fireEvent.click(increaseQuantity);
+  fireEvent.click(increaseQuantity);
+
+  const addToBasketElement = getByText("Add to cart");
+  fireEvent.click(addToBasketElement);
+
+  fireEvent.click(increaseQuantity);
+  fireEvent.click(increaseQuantity);
+  fireEvent.click(increaseQuantity);
+  fireEvent.click(addToBasketElement);
+
+  const basketItems = getByTitle("Basket items");
+  expect(basketItems).toHaveTextContent("8");
+});
+
